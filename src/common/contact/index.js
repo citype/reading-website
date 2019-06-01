49322106073ebf84c64380092a9668860ca0333f
xihe <citype124@gmail.com>
2019-06-02 00:25:15 +0800

    finish pages

--- src/common/contact/index.js
+++ src/common/contact/index.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import './index.css';
 import Top from '../images/company.png'
 import ContactAbout from '../contactAbout'
-
+import Item1 from '../images/interview.jpg'
+import Item2 from '../images/search.jpg'
+import Item3 from '../images/luyong.jpg'
+import Item4 from '../images/shanggang.jpg'
+import Item5 from '../images/report.jpg'
 class Contact extends Component {
     constructor(props) {
         super(props);
@@ -22,23 +26,23 @@ class Contact extends Component {
                     <div className ="cont-middle-content">
                         <ul className="cont-middle-item">
                             <li className="cont-middle-li">
-                                <img src={Top} />
+                                <img src={Item1} />
                                 <p>基础面试(初试·复试·终试)</p>
                             </li>
                             <li className="cont-middle-li">
-                                <img src={Top} />
+                                <img src={Item2} />
                                 <p>背景调查</p>
                             </li>
                             <li className="cont-middle-li">
-                                <img src={Top} />
+                                <img src={Item3} />
                                 <p>录用</p>
                             </li>
                             <li className="cont-middle-li">
-                                <img src={Top} />
+                                <img src={Item4} />
                                 <p>岗前培训和考核</p>
                             </li>
                             <li className="cont-middle-li">
-                                <img src={Top} />
+                                <img src={Item5} />
                                 <p>持证上岗</p>
                             </li>
                         </ul>
